193717706c6085ebd7a0145930fd76bbb797f717
Gerald Nilles <geraldnilles@gmail.com>
2012-09-20 07:36:33 -0500

    Complete XML elements and cert parsing

--- ofx.py
+++ ofx.py
@@ -3,25 +3,64 @@ import xml.etree.ElementTree as et
 # This is the main function that should be called.
 # bank is a dictonary containing info about the financial institution
 def get_trns(bank):
-    # Create Signon Message
+
+    ### Create Signon Message
     signonmsg = et.Element("SIGNONMSGSRQV1")
     # Create Signon Request and add to signon message
-    sonrq = et.Element("SONRQ") 
-    signonmsg.append(sonrq)
+    sonrq = et.SubElement(signonmsg,"SONRQ")
     # Create Time element and add to signon request
-    dtc = et.Element("DTCLIENT")
+    dtc = et.SubElement(sonrq,"DTCLIENT")
     dtc.text = time.time() # TODO Fix format
-    sonrq.append(dtc)
     # Create User ID Element and add to signon reqeust
-    userid = et.Element("USERID")
+    userid = et.SubElement(sonrq,"USERID")
     userid.text = bank["username"]
-    sonrq.append(userid)
     # Create User Password Eleement and add to signon request
-    userpass = et.Element("USERPASS")
+    userpass = et.SubElement(sonrq,"USERPASS")
     userpass.text = encrypt_password(bank)
-    sonrq.append(userpass)
-    #...
-    
+    # Create Languate Element and add to signon Rq
+    lang = et.SubElement(sonrq,"LANGUAGE")
+    lang.text = "ENG"
+    # Creat FI Eleement
+    fi = et.SubElement(sonrq,"FI")
+    # Creat Org Element and FID Element
+    org = et.SubElement(fi,"ORG")
+    org.text = bank["org"]
+    fid = et.SubElement(fi,"FID")
+    fid.text = bank["fid"]
+    # Create App ID and Version Elements
+    appid = et.SubElement(sonrq,"APPID")
+    appid.text = "MyPyOFX"
+    appver = et.SubElement(sonrq,"APPVER")
+    appver.text = "0001"
+
+    # Create Bank Message
+    bankmsg = et.Element("BANKMSGSRQV1")
+    # Create Statement Transaction Request
+    stmttrnrq = et.SubElement(bankmsg,"STMTTRNRQ")
+    # Create Transaction UID
+    trnuid = et.SubElement(stmttrnrq,"TRNUID")
+    trnuid.text = uuid #TODO FIgureo out a UID
+    # Create Statement Request
+    stmtrq = et.SubElement(stmttrnrq,"STMTRQ")
+    # Create Bank Account Element
+    bankacctfrom = et.SubElement(stmtrq,"BANKACCTFROM")
+    # Create Bank ID element
+    bankid = et.SubElement(bankacctfrom,"BANKID")
+    bankid.text = bank["routing_number"]
+    # Create Account Number element
+    acctid = et.subElement(bankacctfrom,"ACCTID")
+    accit.text = bank["acct_number"]
+    # Create Account Type element
+    accttype = et.SubElement(bankacctfrom,"ACCTTYPE")
+    accttype.text = bank["acct_type"]
+    # Create Include Transaction Element
+    inctran = et.SubElement(stmtrq,"INCTRAN")
+    # Create Include Element
+    include = et.SubElement(inctran,"INCLUDE")
+
+    # Add XML and OFX headers and put messages in an <OFX> element
+    create_ofx_body([signonmsg,bankmsg])    
+
     # Create HTTP connection with
     c = urllib(bank["url"])
     c.request(data)
@@ -69,12 +108,9 @@ def encrypt_password(bank):
 # The argument is a list ofrequest element.  
 # These will be placed inside the OFX element
 def create_ofx_body(reqs):
-    output = """
-<?xml version="1.0"?>
+    output = "<?xml version=\"1.0\"?>\r\n\r\n"
+    output = "<?OFX OFXHEADER=\"200\" VERSION=\"211\" SECURITY=\"TYPE1\" OLDFILEUID=\"NONE\" NEWFILEUID=\"NONE\" ?>\r\n\r\n"
 
-<?OFX OFXHEADER="200" VERSION="211" SECURITY="TYPE1" OLDFILEUID="NONE" NEWFILEUID="NONE" ?>
-
-"""
     root = et.Element("ofx")
     for r in reqs:
         root.append(req)
@@ -83,8 +119,16 @@ def create_ofx_body(reqs):
 
     return output
 
-def parse_ns(data):
-    pass
+def parse_ns_cert(data):
+    m = re.search("MIME type.*?; boundary =(.*?)",data)
 
-def parse_cert(data):
-    pass
+    if (m):
+        sp = data.split(m.group(1))
+    else:
+        print "Cert Not INcluded"
+        return 0
+
+    m = re.search("<NS></NS>",sp[2])
+
+    return {"ns"=m.group(1),"cert":sp[3]}
+
