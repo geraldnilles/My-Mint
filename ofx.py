2a2e90168c60d9011093ce2deb852375326dcccf
Gerald Nilles <geraldnilles@gmail.com>
2012-09-22 13:05:31 -0500

    Attempt to get Challenge script to work

--- ofx.py
+++ ofx.py
@@ -1,4 +1,7 @@
 import xml.etree.ElementTree as et
+import httplib
+import re
+import time
 
 # This is the main function that should be called.
 # bank is a dictonary containing info about the financial institution
@@ -71,18 +74,8 @@ def get_trns(bank):
 
 def encrypt_password(bank):
     # Obtain Server's Profile. This gathers information on the capabilities of the FI server
-    # Establish an SSL connection
-    c = HTTPSConnection(bank["url"])
-    # Create <CHALLENGERQ> request
-    
-    ch = et.Element("CHALLENGERQ")
-    uid = et.Element("USERID")
-    ch.append(uid)
-    body = create_ofx_body(ch)
-    c.request("POST","/??",body,headers) 
-    # Recieve <CHALLENGERS> from server.  
-    r = c.getresponse()
-    data = r.read()
+    # Grab the Challange Data
+    data = get_challenge_data(bank)
     # Separate data.  There should be a plain text OFX resopnse as well as a cert
     NS = parse_ns(data)
     SCert = parse_cert(data)
@@ -108,27 +101,77 @@ def encrypt_password(bank):
 # The argument is a list ofrequest element.  
 # These will be placed inside the OFX element
 def create_ofx_body(reqs):
-    output = "<?xml version=\"1.0\"?>\r\n\r\n"
-    output = "<?OFX OFXHEADER=\"200\" VERSION=\"211\" SECURITY=\"TYPE1\" OLDFILEUID=\"NONE\" NEWFILEUID=\"NONE\" ?>\r\n\r\n"
+    
+    output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    output += "<?OFX OFXHEADER=\"200\" VERSION=\"211\" SECURITY=\"TYPE1\" OLDFILEUID=\"NONE\" NEWFILEUID=\"NONE\" ?>\n"
 
-    root = et.Element("ofx")
+    root = et.Element("OFX")
     for r in reqs:
-        root.append(req)
+        root.append(r)
 
-    output += et.ElementTree.tostring(root)
+    output += et.tostring(root)
 
     return output
 
 def parse_ns_cert(data):
-    m = re.search("MIME type.*?; boundary =(.*?)",data)
-
+    m = re.search("MIME type.*?;\s*boundary\s*=\s*(.*?)",data)
+    #TODO Strip any \r or \n characters
     if (m):
-        sp = data.split(m.group(1))
+        sp = data.split("--"+m.group(1))
     else:
         print "Cert Not INcluded"
         return 0
 
     m = re.search("<NS></NS>",sp[2])
 
-    return {"ns"=m.group(1),"cert":sp[3]}
+    return {"ns":m.group(1),"cert":sp[3]}
+
+## Performas a Chalenge request to get encryption info
+
+def get_challenge_data(bank):
+     # Establish an SSL connection
+    c = httplib.HTTPSConnection(split_url(bank["ofx_url"])[0])
+    # Create <CHALLENGERQ> request
+    sign = et.Element("SIGNONMSGSRQV1")
+    #TODO Add SONRQ with anonymous
+    sonrq = et.SubElement(sign,"SONRQ")
+    dt = et.SubElement(sonrq,"DTCLIENT")
+    dt.text = time.strftime("%Y%m%d%H%M%S")
+    userid = et.SubElement(sonrq,"USERID")
+    userid.text = "anonymous"
+    userpass = et.SubElement(sonrq,"USERPASS")
+    userpass.text = "anonymous"
+    lang = et.SubElement(sonrq,"LANGUAGE")
+    lang.text= "ENG"
+    fi = et.SubElement(sonrq,"FI")
+    org = et.SubElement(fi,"ORG")
+    org.text = bank["org"]
+    fid = et.SubElement(fi,"FID")
+    fid.text = bank["fid"]
+    appid = et.SubElement(sonrq,"APPID")
+    appid.text = "MyPyOFX"
+    appver = et.SubElement(sonrq,"APPVER")
+    appver.text = "0.1"
+    chtrn = et.SubElement(sign,"CHALLENGETRNRQ")
+    ch = et.SubElement(chtrn,"CHALLENGERQ")
+    uid = et.SubElement(ch,"USERID")
+    uid.text = bank["username"]
+    body = create_ofx_body([sign])
+    print body
+    headers = { "Content-Type":"application/x-ofx",
+                "User-Agent":"MyPyOFX 0.1"}
+    c.request("POST",split_url(bank["ofx_url"])[1],body,headers)
+    # Recieve <CHALLENGERS> from server.  
+    r = c.getresponse()
+    data = r.read()
+    c.close()
+
+    return data
+
+# Splits the URL into base and path
+def split_url(url):
+    theRE = "https://(.*?)/(.*)"
+    m = re.search(theRE,url)
+    if m:
+        return (m.group(1),"/"+m.group(2)) 
 
